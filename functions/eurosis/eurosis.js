0d32544f55b0c86441c38e3c5ebd0627af0288b1
Francisco Javier Solano <solanof20@gmail.com>
2024-06-16 15:14:42 -0600

    Update eurosis.js

--- functions/eurosis/eurosis.js
+++ functions/eurosis/eurosis.js
@@ -3,13 +3,26 @@ const path = require('path');
 
 exports.handler = async function(event, context) {
   try {
-    const filePath = path.join(__dirname, 'eurosis.json');
-    const jsonData = fs.readFileSync(filePath, 'utf-8');
-    return {
-      statusCode: 200,
-      body: jsonData,
-    };
+    console.log("Starting function for eurosis.json");
+    const filePath = path.resolve(__dirname, 'eurosis.json');
+    console.log("File path resolved:", filePath);
+    if (fs.existsSync(filePath)) {
+      console.log("File exists");
+      const jsonData = fs.readFileSync(filePath, 'utf-8');
+      console.log("File read successfully");
+      return {
+        statusCode: 200,
+        body: jsonData,
+      };
+    } else {
+      console.error("File does not exist:", filePath);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'File does not exist' }),
+      };
+    }
   } catch (error) {
+    console.error("Error reading file:", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed to load data' }),
