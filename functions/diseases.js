7042154e236841de517524304a3b2c670b95b665
Francisco Javier Solano <solanof20@gmail.com>
2024-06-16 14:33:09 -0600

    Update diseases.js

--- functions/diseases.js
+++ functions/diseases.js
@@ -3,12 +3,24 @@ const path = require('path');
 
 exports.handler = async function(event, context) {
   try {
+    console.log("Starting function for diseases.json");
     const filePath = path.resolve(__dirname, 'diseases.json');
-    const jsonData = fs.readFileSync(filePath, 'utf-8');
-    return {
-      statusCode: 200,
-      body: jsonData,
-    };
+    console.log("File path resolved:", filePath);
+    if (fs.existsSync(filePath)) {
+      console.log("File exists");
+      const jsonData = fs.readFileSync(filePath, 'utf-8');
+      console.log("File read successfully");
+      return {
+        statusCode: 200,
+        body: jsonData,
+      };
+    } else {
+      console.error("File does not exist:", filePath);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'File does not exist' }),
+      };
+    }
   } catch (error) {
     console.error("Error reading file:", error);
     return {
