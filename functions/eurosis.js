54450a4eec199d30d0a3dc8558c2423695e8bf39
Francisco Javier Solano <solanof20@gmail.com>
2024-06-16 12:47:45 -0600

    Add files via upload

---
+++ functions/eurosis.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async function() {
+  const filePath = path.join(__dirname, 'eurosis.json');
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  return {
+    statusCode: 200,
+    body: jsonData,
+  };
+};
