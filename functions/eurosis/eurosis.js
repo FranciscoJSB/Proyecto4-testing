8ce2c6bf74e0bbfb51aa6cb8b542d3646a33b249
Mariana <mfernandezmar3@gmail.com>
2024-06-17 15:26:55 -0600

    Comments and minor style changes

--- functions/eurosis/eurosis.js
+++ functions/eurosis/eurosis.js
@@ -1,31 +1,44 @@
+// Modules import
 const fs = require('fs');
 const path = require('path');
 
+// Asynchronous function exported
 exports.handler = async function(event, context) {
   try {
+    // Messages to locate json file
     console.log("Starting function for eurosis.json");
     const filePath = path.resolve(__dirname, 'eurosis.json');
     console.log("File path resolved:", filePath);
+
+    // Check if the file exists
     if (fs.existsSync(filePath)) {
+      // Read the file
       console.log("File exists");
       const jsonData = fs.readFileSync(filePath, 'utf-8');
       console.log("File read successfully");
+
+      // Returns the file content
       return {
         statusCode: 200,
         body: jsonData,
       };
+    // If the file does not exist
     } else {
+      // No file message
       console.error("File does not exist:", filePath);
+      // Returns error message
       return {
         statusCode: 500,
         body: JSON.stringify({ error: 'File does not exist' }),
       };
     }
   } catch (error) {
+    // Error message while reading file
     console.error("Error reading file:", error);
+    // Returns error message
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed to load data' }),
     };
   }
-};
+}; // End of the function
--- functions/school/school.js
+++ functions/school/school.js
@@ -1,19 +1,25 @@
-
+// Modules import
 const fs = require('fs');
 const path = require('path');
 
+// Asynchronous function exported
 exports.handler = async function(event, context) {
   try {
+    // File path
     const filePath = path.join(__dirname, 'school.json');
+    // Read file
     const jsonData = fs.readFileSync(filePath, 'utf-8');
+
+    // Returns the file content
     return {
       statusCode: 200,
       body: jsonData,
     };
   } catch (error) {
+    // Returns error message
     return {
       statusCode: 500,
       body: JSON.stringify({ error: 'Failed to load data' }),
     };
   }
-};
+}; // End of the function
